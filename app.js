1ea49cbe48ffcd4dfce536e9346bb56e7acec235
Josh Speed <Joshhspeed@Joshs-MacBook-Pro.local>
2020-10-14 21:54:52 +0100

    Require dependencies and set up template

---
+++ app.js
@@ -0,0 +1,42 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+
+app.get("/", function (req, res) {
+
+    var today = new Date();
+
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long",
+        year: "numeric"
+    };
+
+    var day = today.toLocaleDateString("en-us", options);
+
+
+
+    res.render("list", {
+        kindOfDay: day
+    });
+});
+
+app.post("/", function(req, res){
+   console.log(req.body.newItem);
+});
+
+
+
+app.listen(3000, function () {
+    console.log("Server started on port 3000");
+});
