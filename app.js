ddffbf855c26e61e0ab93d7878169c4fc4088e85
Josh Speed <Joshhspeed@joshs-mbp.home>
2020-10-20 11:46:42 +0100

    Add ejs todo post functionality

--- app.js
+++ app.js
@@ -1,42 +1,42 @@
 //jshint esversion:6
 
-const express = require("express");
-const bodyParser = require("body-parser");
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.set("view engine", "ejs");
+let items = [ 'Buy Food', 'Cook Food', 'Eat Food' ];
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.set('view engine', 'ejs');
 
+app.use(bodyParser.urlencoded({ extended: true }));
 
+app.get('/', function(req, res) {
+	let today = new Date();
 
-app.get("/", function (req, res) {
+	let options = {
+		weekday : 'long',
+		day     : 'numeric',
+		month   : 'long',
+		year    : 'numeric'
+	};
 
-    var today = new Date();
+	let day = today.toLocaleDateString('en-us', options);
 
-    var options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long",
-        year: "numeric"
-    };
-
-    var day = today.toLocaleDateString("en-us", options);
-
-
-
-    res.render("list", {
-        kindOfDay: day
-    });
+	res.render('list', {
+		kindOfDay    : day,
+		newListItems : items
+	});
 });
 
-app.post("/", function(req, res){
-   console.log(req.body.newItem);
+app.post('/', function(req, res) {
+	item = req.body.newItem;
+
+	items.push(item);
+
+	res.redirect('/');
 });
 
-
-
-app.listen(3000, function () {
-    console.log("Server started on port 3000");
+app.listen(3000, function() {
+	console.log('Server started on port 3000');
 });
